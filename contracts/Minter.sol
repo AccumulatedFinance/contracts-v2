5d802227bbc610aa3b0b659dd29e519d62825f64
ilzheev <a@lovedesign.ru>
2024-06-03 12:23:46 +0200

    add ERC20MinterWithRedeem

--- contracts/Minter.sol
+++ contracts/Minter.sol
@@ -437,3 +437,43 @@ contract ERC20Minter is BaseMinter {
     }
 
 }
+
+
+// ERC20MinterWithRedeem is ERC20Minter extension for restaking that allows to instantly redeem base token (deposited LST)
+contract ERC20MinterWithRedeem is ERC20Minter {
+
+    using SafeMath for uint256;
+    using SafeTransferLib for IERC20;
+
+    uint256 public redeemFee = 0; // possible fee to cover bridging costs
+    uint256 public constant MAX_REDEEM_FEE = 500; // max redeem fee 500bp (5%)
+
+    constructor(address _baseToken, address _stakingToken) ERC20Minter(_baseToken, _stakingToken) {
+    }
+
+    event UpdateRedeemFee(uint256 _redeemFee);
+    event Redeem(address indexed caller, address indexed receiver, uint256 amount);
+
+    function previewRedeem(uint256 amount) public view virtual returns (uint256) {
+        uint256 feeAmount = amount.mul(redeemFee).div(FEE_DENOMINATOR);
+        uint256 netAmount = amount.sub(feeAmount);
+        return netAmount;
+    }
+
+    function updateRedeemFee(uint256 newFee) public onlyOwner {
+        require(newFee <= MAX_REDEEM_FEE, ">MaxFee");
+        redeemFee = newFee;
+        emit UpdateRedeemFee(newFee);
+    }
+
+    function redeem(uint256 amount, address receiver) public nonReentrant {
+        require(amount > 0, "ZeroRedeem");
+        uint256 redeemAmount = previewRedeem(amount);
+        require(redeemAmount > 0, "ZeroRedeemAmount");
+        stakingToken.safeTransferFrom(address(msg.sender), address(this), amount);
+        stakingToken.burn(amount);
+        baseToken.safeTransferFrom(address(this), receiver, redeemAmount);
+        emit Redeem(address(msg.sender), receiver, amount);
+    }
+
+}
