dc70bed4f92811f78eb7eb4aeb5afdc78767d604
ilzheev <a@lovedesign.ru>
2025-07-13 10:05:56 +0200

    core contracts

---
+++ contracts/lending/wrstCORELendingV100.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.20;
+
+import "../LendingV100.sol";
+
+contract wrstCORELendingV100 is NativeLending {
+
+    constructor(IERC4626 _collateralToken) NativeLending(_collateralToken) {}
+
+}
