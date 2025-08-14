bafe403c1cf8fc89dc2d5108421d5af870059bf1
ilzheev <a@lovedesign.ru>
2025-08-15 00:12:46 +0200

    coti contracts

---
+++ contracts/lending/wstCOTILendingV101.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.20;
+
+import "../LendingV101.sol";
+
+contract wstCOTILendingV101 is NativeLending {
+
+    constructor(IERC4626 _collateralToken, address _lsdMinter) NativeLending(_collateralToken, _lsdMinter) {}
+
+}
