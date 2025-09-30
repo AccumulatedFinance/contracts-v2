9278cb744ad40a59fc8627e3587a51c6ffcc1d71
ilzheev <a@lovedesign.ru>
2025-09-30 22:49:40 +0200

    update wstkub lending

---
+++ contracts/lending/wstKUBLendingV102.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.20;
+
+import "../LendingV102.sol";
+
+contract wstKUBLendingV102 is NativeLending {
+
+    constructor(IERC4626 _collateralToken, address _lsdMinter) NativeLending(_collateralToken, _lsdMinter) {}
+
+}
