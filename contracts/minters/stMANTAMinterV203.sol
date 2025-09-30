7fcb065ae074e8d85e1911ae45ece6ea1874dd0d
ilzheev <a@lovedesign.ru>
2025-09-30 22:50:43 +0200

    upgrade stmanta minter

--- contracts/minters/stMANTAMinterV203.sol
+++ contracts/minters/stMANTAMinterV203.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity ^0.8.20;
 
-import "../MinterV201.sol";
+import "../MinterV203.sol";
 
-contract stMANTAMinterWithdrawal is ERC20MinterWithdrawal {
+contract stMANTAMinterV203 is ERC20MinterWithdrawal {
 
     string public BASE_URI = "https://api.accumulated.finance/v1/nft";
 
