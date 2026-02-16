9d05a025ffa1f6dacdd2f6ca875a1ecb40655d60
ilzheev <a@lovedesign.ru>
2026-02-16 19:18:31 +0100

    liquidator logic

--- contracts/Liquidator.sol
+++ contracts/Liquidator.sol
@@ -1,6 +1,112 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
+
+// ERC4626 interface
+interface IERC4626 is IERC20Metadata {
+    function asset() external view returns (address);
+    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets);
+}
+
 interface IFlashLoanReceiver {
     function onFlashLoan(
         address initiator,
@@ -12,6 +118,135 @@ interface IFlashLoanReceiver {
 
 interface IMinter {
     function flashLoan(address receiver, uint256 amount, bytes calldata data) external returns (bool);
+    function requestWithdrawal(uint256 amount, address receiver) external returns (uint256 withdrawalId);
+    function claimWithdrawal(uint256 withdrawalId, address receiver) external;
+}
+interface ILending {
+    function collateral() external view returns (IERC4626);
+    function liquidate(address user, uint256 debtSharesToCover) external payable;
+}
+
+/// @notice Safe ETH and ERC20 transfer library that gracefully handles missing return values.
+/// @author Solmate (https://github.com/transmissions11/solmate/blob/main/src/utils/SafeTransferLib.sol)
+/// @dev Use with caution! Some functions in this library knowingly create dirty bits at the destination of the free memory pointer.
+/// @dev Note that none of the functions in this library check that a token has code at all! That responsibility is delegated to the caller.
+library SafeTransferLib {
+    /*//////////////////////////////////////////////////////////////
+                             ETH OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function safeTransferETH(address to, uint256 amount) internal {
+        bool success;
+
+        assembly {
+            // Transfer the ETH and store if it succeeded or not.
+            success := call(gas(), to, amount, 0, 0, 0, 0)
+        }
+
+        require(success, "ETH_TRANSFER_FAILED");
+    }
+
+    /*//////////////////////////////////////////////////////////////
+                            ERC20 OPERATIONS
+    //////////////////////////////////////////////////////////////*/
+
+    function safeTransferFrom(
+        IERC20 token,
+        address from,
+        address to,
+        uint256 amount
+    ) internal {
+        require(address(token).code.length != 0, "token does not exist");
+        bool success;
+
+        assembly {
+            // Get a pointer to some free memory.
+            let freeMemoryPointer := mload(0x40)
+
+            // Write the abi-encoded calldata into memory, beginning with the function selector.
+            mstore(freeMemoryPointer, 0x23b872dd00000000000000000000000000000000000000000000000000000000)
+            mstore(add(freeMemoryPointer, 4), from) // Append the "from" argument.
+            mstore(add(freeMemoryPointer, 36), to) // Append the "to" argument.
+            mstore(add(freeMemoryPointer, 68), amount) // Append the "amount" argument.
+
+            success := and(
+                // Set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (can't just be non-zero data), or had no return data.
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // We use 100 because the length of our calldata totals up like so: 4 + 32 * 3.
+                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+                // Counterintuitively, this call must be positioned second to the or() call in the
+                // surrounding and() call or else returndatasize() will be zero during the computation.
+                call(gas(), token, 0, freeMemoryPointer, 100, 0, 32)
+            )
+        }
+
+        require(success, "TRANSFER_FROM_FAILED");
+    }
+
+    function safeTransfer(
+        IERC20 token,
+        address to,
+        uint256 amount
+    ) internal {
+        require(address(token).code.length != 0, "token does not exist");
+        bool success;
+
+        assembly {
+            // Get a pointer to some free memory.
+            let freeMemoryPointer := mload(0x40)
+
+            // Write the abi-encoded calldata into memory, beginning with the function selector.
+            mstore(freeMemoryPointer, 0xa9059cbb00000000000000000000000000000000000000000000000000000000)
+            mstore(add(freeMemoryPointer, 4), to) // Append the "to" argument.
+            mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument.
+
+            success := and(
+                // Set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (can't just be non-zero data), or had no return data.
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
+                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+                // Counterintuitively, this call must be positioned second to the or() call in the
+                // surrounding and() call or else returndatasize() will be zero during the computation.
+                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+            )
+        }
+
+        require(success, "TRANSFER_FAILED");
+    }
+
+    function safeApprove(
+        IERC20 token,
+        address to,
+        uint256 amount
+    ) internal {
+        require(address(token).code.length != 0, "token does not exist");
+        bool success;
+
+        assembly {
+            // Get a pointer to some free memory.
+            let freeMemoryPointer := mload(0x40)
+
+            // Write the abi-encoded calldata into memory, beginning with the function selector.
+            mstore(freeMemoryPointer, 0x095ea7b300000000000000000000000000000000000000000000000000000000)
+            mstore(add(freeMemoryPointer, 4), to) // Append the "to" argument.
+            mstore(add(freeMemoryPointer, 36), amount) // Append the "amount" argument.
+
+            success := and(
+                // Set success to whether the call reverted, if not we check it either
+                // returned exactly 1 (can't just be non-zero data), or had no return data.
+                or(and(eq(mload(0), 1), gt(returndatasize(), 31)), iszero(returndatasize())),
+                // We use 68 because the length of our calldata totals up like so: 4 + 32 * 2.
+                // We use 0 and 32 to copy up to 32 bytes of return data into the scratch space.
+                // Counterintuitively, this call must be positioned second to the or() call in the
+                // surrounding and() call or else returndatasize() will be zero during the computation.
+                call(gas(), token, 0, freeMemoryPointer, 68, 0, 32)
+            )
+        }
+
+        require(success, "APPROVE_FAILED");
+    }
 }
 
 /**
@@ -104,18 +339,26 @@ abstract contract Ownable is Context {
     }
 }
 
-contract Liquidator is IFlashLoanReceiver, Ownable {
+contract NativeLiquidator is IFlashLoanReceiver, Ownable {
     bytes32 public constant FLASHLOAN_CALLBACK_SUCCESS =
         keccak256("FLASHLOAN_CALLBACK_SUCCESS");
 
-    address public immutable minter;
+    IMinter public immutable minter;
+    ILending public immutable lending;
+    IERC4626 public immutable asset;
 
-    constructor(address _minter) {
-        minter = _minter;
+    constructor(address _minter, address _lending) {
+        minter = IMinter(_minter);
+        lending = ILending(_lending);
+        asset = lending.collateral();
     }
 
-    function executeFlashLoan(uint256 amount) public onlyOwner {
-        IMinter(minter).flashLoan(address(this), amount, "");
+    function executeFlashLoan(address user, uint256 debtSharesToCover, uint256 amount, address receiver) public onlyOwner {
+        bytes memory data = abi.encode(user, debtSharesToCover);
+        IMinter(minter).flashLoan(address(this), amount, data);
+        // send profits to owner
+        uint256 availableBalance = address(this).balance;
+        SafeTransferLib.safeTransferETH(receiver, availableBalance);
     }
 
     // flashloan contract calls this method to return tokens
@@ -123,14 +366,29 @@ contract Liquidator is IFlashLoanReceiver, Ownable {
         address initiator,
         uint256 amount,
         uint256 fee,
-        bytes calldata /* data */
+        bytes calldata data
     ) external payable returns (bytes32) {
         require(initiator == owner(), "BadInitiator");
-        require(msg.sender == minter, "NotMinter");
+        require(msg.sender == address(minter), "NotMinter");
 
-        require(payable(address(this)).balance >= amount + fee, "NotEnoughToRepay");
-        (bool ok,) = payable(minter).call{value: amount + fee}("");
-        require(ok, "RepayFailed");
+        // liquidate user
+        (address user, uint256 debtSharesToCover) = abi.decode(data, (address, uint256));
+        lending.liquidate{value: amount}(user, debtSharesToCover);
+        
+        // redeem collateral
+        uint256 shares = IERC20(asset).balanceOf(address(this));
+        uint256 assets = asset.redeem(shares, address(this), address(this));
+
+        // withdraw from minter
+        uint256 withdrawalId = minter.requestWithdrawal(assets, address(this));
+        minter.claimWithdrawal(withdrawalId, address(this));
+
+        uint256 availableBalance = address(this).balance;
+        uint256 repayAmount = amount + fee;
+
+        require(availableBalance >= repayAmount, "NotEnoughToRepay");
+        SafeTransferLib.safeTransferETH(address(minter), repayAmount);
+
         return FLASHLOAN_CALLBACK_SUCCESS;
     }
 }
